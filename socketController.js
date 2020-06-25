ce5ee9398fa3f457eac289fc588d32c5d5cfa921
CWD <cwd@cloudspokes.com>
2020-06-25 17:58:52 -0400

    restructuring into 2 distinct channels.
    one for commands and one for messages. idea will be to interpret input on the server side and remote the front

--- socketController.js
+++ socketController.js
@@ -5,19 +5,28 @@ const _ = require('lodash')
 class controller {
   constructor (io) {
     this.io = io
+    this.commandList = [{ command: 'clear', func: this.commandClear.bind(this) }] // CWD-- factor these out to another object perhaps
     this.iScreenHeight = 10
     this.currentLine = 0
 
     io.on('connection', this.connectionHandler.bind(this))
   }
 
+  // CWD-- basic ops
+
   getIO () {
     return this.io
+  }
+
+  sendCommand (msg) {
+    this.io.emit('cmd', msg)
   }
 
   sendMessage (msg) {
     this.io.emit('msg', msg)
   }
+
+  // CWD-- connection handling
 
   connectionHandler (socket) {
     console.log('a user connected')
@@ -34,12 +43,40 @@ class controller {
     console.log('user disconnected')
   }
 
-  commandHandler (msg) {
-    console.log('received command:', msg)
+  // CWD-- interaction handling
 
-    switch (_.get(msg, 'op', '')) {
+  messageHandler (msgIn) {
+    console.log('incoming txt: ' + msgIn.txt)
+    const cmdFunc = this.findCommand(msgIn)
+    let blnContinue = true
+
+    console.log(cmdFunc)
+
+    if (cmdFunc) {
+      blnContinue = cmdFunc.func(msgIn)
+    }
+
+    if (blnContinue) {
+      const msgOut = { txt: msgIn.txt, op: 'text', y: this.currentLine, x: 0 }
+
+      if (this.currentLine > this.iScreenHeight) {
+        this.currentLine = -1
+      }
+
+      ++this.currentLine
+      msgOut.y = this.currentLine
+      this.sendMessage(msgOut)
+    }
+
+    console.log(this.currentLine)
+  }
+
+  commandHandler (msgIn) {
+    console.log('received command:', msgIn)
+
+    switch (_.get(msgIn, 'op', '')) {
       case 'setScreenSize': {
-        this.iScreenHeight = _.get(msg, 'val.height', this.iScreenHeight)
+        this.iScreenHeight = _.get(msgIn, 'val.height', this.iScreenHeight)
         console.log(`setting screen size: ${this.iScreenHeight}`)
         break
       }
@@ -49,27 +86,25 @@ class controller {
     }
   }
 
-  messageHandler (msgIn) {
-    console.log('incoming txt: ' + msgIn.txt)
-    const msgOut = { txt: msgIn.txt, op: 'text', y: this.currentLine, x: 0 }
+  // CWD-- command funcs. perhaps refactor to own class
 
-    if (msgIn.txt === 'clear') {
-      this.currentLine = 0
-      msgOut.op = 'clear'
-      console.log(`currentLine reached ${this.currentLine}. clearing screen`)
-      this.sendMessage(msgOut)
-    }
-
-    if (this.currentLine > this.iScreenHeight) {
-      this.currentLine = -1
-    }
-
-    ++this.currentLine
-    msgOut.y = this.currentLine
-    this.sendMessage(msgOut)
-
-    console.log(this.currentLine)
+  findCommand (cmd) {
+    // CWD-- TODO: parse out command text. Assume single word commands for now
+    console.log('looking for valid commands: ', cmd, this.commandList)
+    return _.find(this.commandList, ['command', cmd.txt])
   }
+
+  commandClear (msgIn) {
+    this.currentLine = -1
+    const msgOut = { txt: msgIn.txt, op: 'text', y: this.currentLine, x: 0 }
+    msgOut.op = 'clear'
+    console.log(`currentLine reached ${this.currentLine}. clearing screen`)
+    this.sendCommand(msgOut)
+    // this.sendMessage(msgOut)
+    return false
+  }
+
+
 }
 
 module.exports = controller
